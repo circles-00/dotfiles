4191675d763c3f0414740b30cdc826cf74653181
Nikola Krezeski <nkrezeski@banzae.dev>
2023-09-15 15:32:09 +0200

    initial commit

---
+++ nvim/after/plugin/colors.lua
@@ -0,0 +1,15 @@
+require('rose-pine').setup({
+  disable_background = true
+})
+
+local colorScheme = "github_dark_tritanopia"
+
+function ColorMyPencils(color)
+  color = color or "rose-pine"
+  vim.cmd.colorscheme(color)
+
+ -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+ -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
+ColorMyPencils(colorScheme)
