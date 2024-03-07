0651ef7fcc64074e0919a0a069a15e4b4e7ebf8f
Nikola Krezeski <nkrezeski@banzae.dev>
2024-03-08 00:21:05 +0100

    refactor: change tmux prefix

--- .config/nvim/lua/darknet/remap.lua
+++ .config/nvim/lua/darknet/remap.lua
@@ -36,7 +36,7 @@ vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "Q", "<nop>")
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+-- vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>f", function()
   vim.lsp.buf.format()
   local currentFileName = vim.fn.expand("%:t")
