046c10228ebbbd59c1458f7d1332ff4dba1414bd
Nikola Krezeski <nkrezeski@banzae.dev>
2024-03-12 04:05:32 +0100

    feat: change nvim status bar

--- .config/nvim/lua/darknet/packer.lua
+++ .config/nvim/lua/darknet/packer.lua
@@ -95,6 +95,8 @@ return require('packer').startup(function(use)
     run = 'cd formatter && npm i && npm run build',
   }
 
+  --  use('~/CodingProjects/personal/nvim-discord-status') -- TODO: Change to use the actual repo
+
   use("circles-00/nvim-discord-status")
   --  use("stevearc/conform.nvim")
 end)
