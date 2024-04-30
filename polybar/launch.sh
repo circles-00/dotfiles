23f13ef861a0776d0fcfe4442e61ba139a7b20bb
Nikola Krezeski <nkrezeski@banzae.dev>
2024-04-30 04:03:28 +0200

    fix: dunst not starting

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -9,6 +9,4 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch Polybar, using default config location ~/.config/polybar/config
 polybar example &
 
-pkill dunst && dunst&
-
 echo "Polybar launched..."
