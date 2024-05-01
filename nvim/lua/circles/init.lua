fcae7e730f32c23be618ca6df07dacfd774f0fe9
Nikola Krezeski <nkrezeski@banzae.dev>
2024-05-01 22:09:21 +0200

    feat(nvim): migrate packer to lazy

--- nvim/lua/circles/init.lua
+++ nvim/lua/circles/init.lua
@@ -1,13 +1,13 @@
-require("darknet.set")
-require("darknet.remap")
-local statusLine = require("darknet.status_line")
+require("circles.set")
+require("circles.remap")
+local statusLine = require("circles.status_line")
 
 -- DO NOT INCLUDE THIS
 -- vim.opt.rtp:append("~/personal/streamer-tools")
 -- DO NOT INCLUDE THIS
 
 local augroup = vim.api.nvim_create_augroup
-local ThePrimeagenGroup = augroup('ThePrimeagen', {})
+local CirclesGroup = augroup('circles', {})
 
 local autocmd = vim.api.nvim_create_autocmd
 local yank_group = augroup('HighlightYank', {})
@@ -28,7 +28,7 @@ autocmd('TextYankPost', {
 })
 
 autocmd({ "BufWritePre" }, {
-  group = ThePrimeagenGroup,
+  group = CirclesGroup,
   pattern = "*",
   command = [[%s/\s\+$//e]],
 })
@@ -39,5 +39,3 @@ vim.g.netrw_winsize = 25
 
 statusLine.setup()
 
--- Autoclose
-require("autoclose").setup()
---
+++ nvim/lua/plugins/init.lua
@@ -0,0 +1,13 @@
+return {
+  'nvim-treesitter/playground',
+  'eandrju/cellular-automaton.nvim',
+  'laytan/cloak.nvim',
+  -- use 'github/copilot.vim',
+  'airblade/vim-gitgutter',
+  'f-person/git-blame.nvim',
+  'projekt0n/github-nvim-theme',
+  'ThePrimeagen/vim-be-good',
+  'windwp/nvim-ts-autotag',
+ -- use '~/CodingProjects/personal/nvim-discord-status', -- TODO: Change to use the actual repo
+ -- use 'circles-00/nvim-discord-status'
+}
