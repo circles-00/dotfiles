fb0bd5230087845d77fc1c5a21d12b09bcfec0d0
Nikola Krezeski <nkrezeski1@gmail.com>
2024-07-03 10:28:20 +0200

    change lualine, lazy load neotree change keybind

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -6,7 +6,9 @@ return {
       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
       "MunifTanjim/nui.nvim",
     },
-    config = function ()
-      vim.keymap.set('n', '<C-b>', '<Cmd>Neotree toggle right<CR>')
-    end
+    cmd = "Neotree",
+    lazy = true,
+    keys = {
+      { "<C-b>", "<cmd>Neotree reveal right<cr>" },
+    },
 }
