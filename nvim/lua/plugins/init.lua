61aa0114ef1a2ff8e0370e6bfe80a0d8a613d472
Nikola Krezeski <nkrezeski1@gmail.com>
2024-07-05 10:56:35 +0200

    change polybar colors

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,11 +1,5 @@
 return {
   'nvim-treesitter/playground',
   'eandrju/cellular-automaton.nvim',
-  'laytan/cloak.nvim',
-  -- use 'github/copilot.vim',
-  'projekt0n/github-nvim-theme',
   'ThePrimeagen/vim-be-good',
-  'windwp/nvim-ts-autotag',
- -- use '~/CodingProjects/personal/nvim-discord-status', -- TODO: Change to use the actual repo
- -- use 'circles-00/nvim-discord-status'
 }
