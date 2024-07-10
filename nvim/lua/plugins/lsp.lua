8ce8ecbf2e8e12a35b2eaf8391cf7e7048d72b75
Nikola Krezeski <nkrezeski1@gmail.com>
2024-07-10 02:26:45 +0200

    change snippets dir, take symlink instead of relative path

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -39,7 +39,7 @@ return {
     local luasnip = require("luasnip")
 
     require("luasnip.loaders.from_vscode").lazy_load()
-    require("luasnip.loaders.from_vscode").lazy_load({ paths = "~/CodingProjects/personal/dotfiles/nvim/snippets/" })
+    require("luasnip.loaders.from_vscode").lazy_load({ paths = "~/.config/nvim/snippets/" })
 
     local cmp = require('cmp')
     cmp.setup {
