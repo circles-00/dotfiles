ba137137fc89e173ef58d94a590dd21c8fc8011a
Nikola Krezeski <nkrezeski1@gmail.com>
2024-07-11 01:44:59 +0200

    refactor polybar spotify script

--- polybar/spotify.sh
+++ polybar/spotify.sh
@@ -1,29 +1,22 @@
-#!/bin/sh
+#!/bin/zsh
 
 main() {
-  # TODO: REmove once fully switched from spotify client
-  #
-  # if ! pgrep -x spotify >/dev/null; then
-  #   echo ""; exit
-  # fi
-  #
-  # cmd="org.freedesktop.DBus.Properties.Get"
-  # domain="org.mpris.MediaPlayer2"
-  # path="/org/mpris/MediaPlayer2"
-  #
-  # meta=$(dbus-send --print-reply --dest=${domain}.spotify \
-  #   /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:${domain}.Player string:Metadata)
-  #
-  # artist=$(echo "$meta" | sed -nr '/xesam:artist"/,+2s/^ +string "(.*)"$/\1/p' | tail -1  | sed 's/\&/\\&/g' | sed 's#\/#\\/#g')
-  # album=$(echo "$meta" | sed -nr '/xesam:album"/,+2s/^ +variant +string "(.*)"$/\1/p' | tail -1| sed 's/\&/\\&/g'| sed 's#\/#\\/#g')
-  # title=$(echo "$meta" | sed -nr '/xesam:title"/,+2s/^ +variant +string "(.*)"$/\1/p' | tail -1 | sed 's/\&/\\&/g'| sed 's#\/#\\/#g')
-  #
-  # echo "${*:-%artist% - %title%}" | sed "s/%artist%/$artist/g;s/%title%/$title/g;s/%album%/$album/g"i | sed "s/\&/\&/g" | sed "s#\/#\/#g"
-  #
-  song_title=$(playerctl -p spotifyd metadata title)
-  song_artist=$(playerctl -p spotifyd metadata artist)
+  PLAYER="spotifyd"
 
-  echo "$song_title - $song_artist"
+  player_status=$(playerctl --player=$PLAYER status)
+
+  # TODO: I can probably merge these 2 if statements, but I'm not a hardcode basher, so imma leave it like this
+  if ! pgrep -x $PLAYER >/dev/null; then
+    echo ""; exit
+  fi
+
+  if [ $player_status != "Playing" ]; then
+    echo ""; exit
+  fi
+
+  song_details=$(playerctl metadata $PLAYER --format "{{ title }} - {{ artist }}")
+
+  echo "$song_details"
 }
 
 main "$@"
