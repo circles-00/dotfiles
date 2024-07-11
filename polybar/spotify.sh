0bb78f6f362c37893bbcedba433ab7d80657a37e
Nikola Krezeski <nkrezeski1@gmail.com>
2024-07-11 11:46:50 +0200

    refactor spotify script, make it extensible to work with any player, use dbus

--- polybar/spotify.sh
+++ polybar/spotify.sh
@@ -1,22 +1,25 @@
-#!/bin/zsh
+#!/bin/sh
 
 main() {
   PLAYER="spotifyd"
+  player_dbus=$(dbus-send --print-reply --dest=org.freedesktop.DBus  /org/freedesktop/DBus org.freedesktop.DBus.ListNames | grep $PLAYER | awk '{print $2}' | tr -d '"')
 
-  player_status=$(playerctl --player=$PLAYER status)
-
-  # TODO: I can probably merge these 2 if statements, but I'm not a hardcode basher, so imma leave it like this
   if ! pgrep -x $PLAYER >/dev/null; then
     echo ""; exit
   fi
 
-  if [ $player_status != "Playing" ]; then
-    echo ""; exit
-  fi
+  cmd="org.freedesktop.DBus.Properties.Get"
+  domain="org.mpris.MediaPlayer2"
+  path="/org/mpris/MediaPlayer2"
 
-  song_details=$(playerctl metadata $PLAYER --format "{{ title }} - {{ artist }}")
+  meta=$(dbus-send --print-reply --dest=${player_dbus} \
+    /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:${domain}.Player string:Metadata)
 
-  echo "$song_details"
+  artist=$(echo "$meta" | sed -nr '/xesam:artist"/,+2s/^ +string "(.*)"$/\1/p' | tail -1  | sed 's/\&/\\&/g' | sed 's#\/#\\/#g')
+  album=$(echo "$meta" | sed -nr '/xesam:album"/,+2s/^ +variant +string "(.*)"$/\1/p' | tail -1| sed 's/\&/\\&/g'| sed 's#\/#\\/#g')
+  title=$(echo "$meta" | sed -nr '/xesam:title"/,+2s/^ +variant +string "(.*)"$/\1/p' | tail -1 | sed 's/\&/\\&/g'| sed 's#\/#\\/#g')
+
+  echo "${*:-%artist% - %title%}" | sed "s/%artist%/$artist/g;s/%title%/$title/g;s/%album%/$album/g"i | sed "s/\&/\&/g" | sed "s#\/#\/#g"
 }
 
 main "$@"
