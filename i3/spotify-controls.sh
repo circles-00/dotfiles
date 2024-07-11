da45be8d8297935d516353b964c2122b3df588aa
Nikola Krezeski <nkrezeski1@gmail.com>
2024-07-11 11:56:50 +0200

    refactor(i3): replace media controls with dbus commands as they are blazingly fast

---
+++ i3/spotify-controls.sh
@@ -0,0 +1,19 @@
+if [ -z "$1" ]
+then
+    echo "Usage: MediaPlayer2-cmd { PlayPause | Next | Previous | Stop | ... }"
+    exit 1
+fi
+
+
+PLAYER="spotifyd"
+player_dbus=$(dbus-send --print-reply --dest=org.freedesktop.DBus  /org/freedesktop/DBus org.freedesktop.DBus.ListNames | grep $PLAYER | awk '{print $2}' | tr -d '"')
+
+if ! pgrep -x $PLAYER >/dev/null; then
+  echo ""; exit
+fi
+
+dbus-send \
+    --print-reply \
+    --dest=$player_dbus \
+    /org/mpris/MediaPlayer2 \
+    org.mpris.MediaPlayer2.Player.$1
