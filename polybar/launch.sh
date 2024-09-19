6c741f4d5371441f4d635204c5638631954aa7fa
Nikola Krezeski <nkrezeski1@gmail.com>
2024-09-19 11:45:30 +0200

    feat: add keyboard layout info to polybar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Terminate already running bar instances
-# killall -q polybar
-kill -9 $(pgrep -f 'polybar') >/dev/null 2>&1
+killall -q polybar
+# kill -9 $(pgrep -f 'polybar') >/dev/null 2>&1
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
